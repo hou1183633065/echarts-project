793777d34af1d9c41631f41d089fc2600ab84042
houzhiqiang <zhq.hou@muheda.com>
2018-11-08 09:55:55 +0800

    修改router

---
+++ src/views/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="indexPage">
+    <div id="nav">
+      <router-link to="/home">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
