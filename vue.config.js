583076165751af33244bf146262531d3a75d9887
houzhiqiang <zhq.hou@muheda.com>
2018-11-08 09:55:38 +0800

    配置vue.config

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  assetsDir: 'static',
+  baseUrl: '/charts/',
+  outputDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: {
+    sourceMap: true
+  }
+}
