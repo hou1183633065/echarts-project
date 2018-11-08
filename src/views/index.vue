e8c2ed630c5185bbeca248c05fd02418b5430daf
houzhiqiang <zhq.hou@muheda.com>
2018-11-08 19:13:34 +0800

    修改router-link到index.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,7 +2,8 @@
   <div class="indexPage">
     <div id="nav">
       <router-link to="/home">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/charts">Charts</router-link>
     </div>
   </div>
 </template>
